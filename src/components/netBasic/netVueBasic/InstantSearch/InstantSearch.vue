2a832a04a08664b561bd181a5781fc0ef0d6f4c6
zhengyekun <1634372267@qq.com>
2018-08-29 18:34:07 +0800

    yhgas

---
+++ src/components/netBasic/netVueBasic/InstantSearch/InstantSearch.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2 class="text-center">{{title}}</h2>
+    <ul class="text-center spacing" style="padding: 0">
+      <li v-for="item in lisks">
+        <a @click="$goRoute(item.route)">{{item.text}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "InstantSearch",
+      data() {
+        return {
+          title: '这是InstantSearch页面-netVueBasic下的即时搜索页面',
+          lisks: [
+            {
+              text: 'InstantSearch',
+              route: '/netBasic/InstantSearch'
+            },
+            {
+              text: 'end',
+              route: '/netBasic/end'
+            },
+            {
+              text: 'return',
+              route: '/Home'
+            }
+          ],
+        }
+
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/netBasic/netVueBasic/InstantSearch/end/end.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2 class="text-center">{{title}}</h2>
+    <ul class="text-center spacing" style="padding: 0">
+      <li v-for="item in lisks">
+        <a @click="$goRoute(item.route)">{{item.text}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "end",
+      lisks: [
+        {
+          text: 'end',
+          route: '/netBasic/end'
+        },
+        {
+          text: 'return',
+          route: '/Home'
+        }
+      ],
+    }
+</script>
+
+<style scoped>
+
+</style>
